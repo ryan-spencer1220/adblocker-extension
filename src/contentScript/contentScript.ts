2348460dae5a3c8f3d912d269322aeb44b57c083
Your Name <youremail@example.com>
2023-09-14 14:56:33 -0700

    complete extension - 1st iteration

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,3 +1,25 @@
-chrome.runtime.sendMessage("Hello from content script!", (response) => {
-  console.log(response);
-});
+const rules: {
+  [url: string]: () => void;
+} = {
+  "https://www.nytimes.com/section/technology": filterNYTTechnology2,
+};
+
+function filterNYTTechnology() {
+  const app = document.getElementById("site-content");
+  const wrapper = document.createElement("top-wrapper");
+  app.removeChild(wrapper);
+}
+
+function filterNYTTechnology2() {
+  const divs = document.getElementsByTagName("div");
+  for (const div of divs) {
+    if (div.className.indexOf("ad") == -1) {
+      div.style.display = "none";
+    }
+  }
+}
+
+if (document.URL in rules) {
+  console.log(document.URL);
+  rules[document.URL]();
+}
